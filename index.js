41bab703d0b4a75cf8e4cbc14a83f8179361e278
edtromp-tech <67668853+edtromp-tech@users.noreply.github.com>
2023-12-30 10:22:35 -0600

    Update index.js with additional filters
    ram
    processor

--- index.js
+++ index.js
@@ -6,7 +6,9 @@
       const priceFilter = getCheckedCheckboxes('price');
 	  const memFilter = getCheckedCheckboxes('mem');
       const clockFilter = getCheckedCheckboxes('clock');
-
+	  const processorFilter = getCheckedCheckboxes('processor');
+	  const ramFilter = getCheckedCheckboxes('ram');
+	
       // Iterate through each GPU row
       const gpuRows = document.querySelectorAll('.gpu');
       gpuRows.forEach(function (row) {
@@ -14,15 +16,19 @@
         const price = row.getAttribute('data-price');
 		const mem = row.getAttribute('data-mem');
         const clock = row.getAttribute('data-clock');
+		const processor = row.getAttribute('data-processor');
+        const ram = row.getAttribute('data-ram');
 
         // Check if the row meets the filter criteria
         const brandMatch = brandFilter.includes(brand) || brandFilter.length === 0;
         const priceMatch = priceFilter.includes(price) || priceFilter.length === 0;
 		const memMatch = memFilter.includes(mem) || memFilter.length === 0;
         const clockMatch = clockFilter.includes(clock) || clockFilter.length === 0;
+		const processorMatch = processorFilter.includes(processor) || processorFilter.length === 0;
+        const ramMatch = ramFilter.includes(ram) || ramFilter.length === 0;
 
         // Show or hide the row based on filter criteria
-        row.style.display = brandMatch && priceMatch && memMatch && clockMatch ? '' : 'none';
+        row.style.display = brandMatch && priceMatch && memMatch && clockMatch && ramMatch && processorMatch ? '' : 'none';
       });
     }
 
@@ -35,7 +41,7 @@
     }
 
     // Attach change event listeners to the brand and price checkboxes
-    document.querySelectorAll('[data-brand], [data-price], [data-mem], [data-clock]').forEach(function (checkbox) {
+    document.querySelectorAll('[data-brand], [data-price], [data-mem], [data-clock], [data-ram], [data-processor]').forEach(function (checkbox) {
       checkbox.addEventListener('change', updateTable);
     });
   });
